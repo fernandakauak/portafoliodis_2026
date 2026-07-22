4f4b2f3f9c1422c7ff373818a7efb0cf332a4b38
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-22 11:36:38 -0400

    Sección conocimiento

--- src/components/AboutComp.vue
+++ src/components/AboutComp.vue
@@ -7,45 +7,57 @@
         <p>Tengo estudios en marketing digital, gestión de proyectos y actualmente me estoy especializando en el área de desarrollo front-end para la creación de aplicaciones digitales.</p>
     </section>
     
-    <article class="conocimiento">
-        <h2>Conocimiento Web</h2>
-        <div class="estadistica">
-            <div class="pcnt90">HTML</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt95">CSS3</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt70">JavaScript</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt70">VueJS</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt90">Diseño Responsivo</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt90">Bootstrap</div>
-        </div>
-        <div class="estadistica">
-            <div class="pcnt80">Wordpress</div>
-        </div>
-        <p>Manejo de FTP</p>
-        <div class="estadistica">
-            <div class="pcnt90">Git</div>
-        </div>
-    </article>
+    <section class="datos">
+        <article class="conocimiento">
+            <h2>Conocimiento Web</h2>
+            <div class="estadistica">
+                <div class="pcnt90">HTML</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt95">CSS3</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt70">JavaScript</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt70">VueJS</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">Diseño Responsivo</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">Bootstrap</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt80">Wordpress</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt75">Git</div>
+            </div>
+        </article>
 
-    <article class="conocimiento">
-        <h2>Conocimiento Software</h2>
-        <p>Illustrator</p>
-        <p>Photoshop</p>
-        <p>InDesign</p>
-        <p>Visual Studio Code</p>
-        <p>Figma</p>
-        <p>Microsoft Office</p>
-        <p>Xampp</p>
-    </article>
+        <article class="conocimiento">
+            <h2>Conocimiento Software</h2>
+            <div class="estadistica">
+                <div class="pcnt95">Illustrator</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">Photoshop</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">InDesign</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">Visual Studio Code</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt75">Figma</div>
+            </div>
+            <div class="estadistica">
+                <div class="pcnt90">Microsoft Office</div>
+            </div>
+        </article>
+    </section>
 
     <aside>
         <h2>¿Quieres conversar?</h2>
@@ -55,10 +67,21 @@
 </template>
 
 <style scope>
+    .datos {
+        display: flex;
+    }
+
     .conocimiento {
-        background-color: rgb(139, 90, 137);
         width: 48%;
         margin: 1%;
+    }
+
+    .conocimiento h2 {
+        margin: 0;
+        padding: 2%;
+        border-radius: 20px 20px 0 0;
+        background-color: darkblue;
+        color: white;
     }
 
     .conocimiento .estadistica {
@@ -67,7 +90,7 @@
         border: 1px solid  rgb(15, 8, 48);
     }
 
-    .estadistica .pcnt95, .estadistica .pcnt90, .estadistica .pcnt80, .estadistica .pcnt70 {
+    .estadistica .pcnt95, .estadistica .pcnt90, .estadistica .pcnt80, .estadistica .pcnt75, .estadistica .pcnt70 {
         background-color: blue;
         color: white;
         border: 1px solid darkblue;
@@ -85,6 +108,10 @@
     .pcnt80 {
         width: 80%;
     }
+
+    .pcnt75 {
+        width: 75%;
+    }    
 
     .pcnt70 {
         width: 70%;
